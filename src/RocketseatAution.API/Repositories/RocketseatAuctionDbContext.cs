a9b357116bda3a1c7752f1c2c7a47e78fb895785
Oscar Lima Oliveira Junior <oscarlojr@gmail.com>
2024-02-08 21:23:25 -0300

    feat: Implementing the project dependencies

---
+++ src/RocketseatAution.API/Repositories/RocketseatAuctionDbContext.cs
@@ -0,0 +1,9 @@
+﻿using System.Data.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace RocketseatAution.API.Repositories;
+
+public class RocketseatAuctionDbContext : DbContext
+{
+
+}
--- src/RocketseatAution.API/obj/Debug/net8.0/RocketseatAution.API.AssemblyInfo.cs
+++ src/RocketseatAution.API/obj/Debug/net8.0/RocketseatAution.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RocketseatAution.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80745878acbdd4c91dfee8fffc30ace23c80e10e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34c30c450de4122b65d081d632f8302f36a782d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("RocketseatAution.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RocketseatAution.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
