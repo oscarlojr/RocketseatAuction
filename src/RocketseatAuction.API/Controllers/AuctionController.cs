f423336c05286fa00766c33d9a5cb6beb6255b8e
Oscar Lima Oliveira Junior <oscarlojr@gmail.com>
2024-02-10 08:07:20 -0300

    feat: Completing the first class

--- src/RocketseatAuction.API/Controllers/AuctionController.cs
+++ src/RocketseatAuction.API/Controllers/AuctionController.cs
@@ -11,11 +11,15 @@ public class AuctionController : ControllerBase
 {
   [HttpGet]
   [ProducesResponseType(typeof(Auction), StatusCodes.Status200OK)]
+  [ProducesResponseType(StatusCodes.Status204NoContent)]
   public IActionResult GetCurrentAuction()
   {
     var useCase = new GetCurrentAuctionUseCase();
     
     var result = useCase.Execute();
+
+    if(result is null) 
+      return NoContent();
     return Ok(result);
   }
 }
--- src/RocketseatAuction.API/UseCases/Auctions/GetCurrent/GetCurrentAuctionUseCase.cs
+++ src/RocketseatAuction.API/UseCases/Auctions/GetCurrent/GetCurrentAuctionUseCase.cs
@@ -6,12 +6,15 @@ namespace RocketseatAuction.API;
 
 public class GetCurrentAuctionUseCase
 {
-  public Auction Execute()
+  public Auction? Execute()
   {
     var repository = new RocketseatAuctionDbContext();
+
+    var today = DateTime.Now;
+
     return repository
           .Auctions
           .Include(auction => auction.Items)
-          .First();
+          .FirstOrDefault(auction => today >= auction.Starts && today <= auction.Ends);
   }
 }
